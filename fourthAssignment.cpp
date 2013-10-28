552129c591f5600307d1fbb245076d7c8183293f
grosalex <alexbruneau93@gmail.com>
2013-10-28 14:28:22 -0400

    initial commit

---
+++ fourthAssignment.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : fourthAssignment.cpp
+ Author      : grosalex
+ Version     :
+ Copyright   : do what you want with it
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "This is the fourth Assignment" << endl; /* prints This is the fourth Assignment */
+	return 0;
+}
