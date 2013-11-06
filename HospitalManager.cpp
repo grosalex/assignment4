10fffdf41b9167f83ac34ef1fb69c3fde14632ac
williamarques91@gmail.com <williamarques91@gmail.com>
2013-11-06 15:12:24 -0500

    Added patient_discharge and addemployee in HospitalManager class.

--- HospitalManager.cpp
+++ HospitalManager.cpp
@@ -30,9 +30,42 @@ bool HospitalManager::patient_admission(Patient* patient, int physician_id) {
 }
 
 bool HospitalManager::patient_discharge(int patient_nb) {
+	bool ok = false;
+	for(int i=0; i<maxbed;i++) {
+		if(patientArray[i]!=NULL)
+			if (patient_nb == dynamic_cast <Patient*> (patientArray[i])->getPatientNumber())
+			{
+				cout << "Found patient at index " << i << " in patient array." << endl;
+				for(int j=0; j<archivesize;j++) {
+					if(patientdischargedArray[j] == NULL) {
+						patientdischargedArray[j] = patientArray[i]
+						ok = true;
+						cout << "Patient transfered in patient discharged array at index " << j << endl;
+					}
+				}
+
+				delete(patientArray[i]);
+				patientArray[i] = NULL;
+				i = maxbed; //leaving for loop
+			}
+	}
+	if(ok) cout << "Patient discharged successfully." << endl;
+	else cout << "Patient no discharged." << endl;
+
+	return ok;
 }
 
 bool HospitalManager::add_employee(Personnel* employee) {
+	bool ok=false;
+	for(int i=0; i<maxperson;i++) {
+		if(personnelArray[i]==NULL) {
+			personnelArray[i] = employee;
+			cout << "Employee added at index " << i <<" in personnel array." << endl;
+			ok = true;
+			i = maxperson;
+		}
+	}
+	return ok;
 }
 
 bool HospitalManager::remove(int employee_nb) {
