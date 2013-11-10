81224ecb6bb77eff3d2072f1b517cff3222c1356
grosalex <alexbruneau93@gmail.com>
2013-11-10 15:19:17 -0500

    adding the add employee feature

--- fourthAssignment.cpp
+++ fourthAssignment.cpp
@@ -9,11 +9,16 @@
  */
 
 #include <iostream>
-
+#include "HospitalManager.h"
 using namespace std;
 
 int main(void) {
-	/*1 - add employee
+	char choice=' ';
+	char createChoice=' ';
+	bool check=false;
+	HospitalManager myManager;
+	/*
+1 - add employee
 
 2 - delete employee : ask for employee id, remove from employee id
 
@@ -27,13 +32,55 @@ int main(void) {
 5 - print physicians()
 6 - print my patient (sending physician id parameter)
 7 - print earnings (int employeeid)*/
+	do{
+		cout << "1 - Add employee" << endl;
+		cout << "2 - Delete employee" << endl;
+		cout << "3 - Add Patient" << endl;
+		cout << "4 - Discharge patient" << endl;
+		cout << "5 - Print physicians" << endl;
+		cout << "6 - Print patient's physicians" << endl;
+		cout << "7 - Print employee earnings" << endl;
+		cout << "q - To quit" << endl;
+		cin >> choice;
+		switch(choice){
+		case '1':
+				cout << "To add a nurse enter n. To add a staff enter s. to add a physician enter p.";
+				cin >> createChoice;
+				switch(createChoice){
+				case'n':
+					check=myManager.add_employee(new Nurses());
+					break;
+				case's':
+					check=myManager.add_employee(new Staff());
+					break;
+				case'p':
+					check=myManager.add_employee(new Physicians());
+					break;
+				}
+				if(check){
+					cout << "Employee correctly added"<< endl;
+				}
+				else{
+					cout << "Couldn't added the employee" << endl;
+				}
+				createChoice=' ';
+			break;
+		case '2':
 
-	cout << "1 - Add employee" << endl;
-	cout << "2 - Delete employee" << endl;
-	cout << "3 - Add Patient" << endl;
-	cout << "4 - Discharge patient" << endl;
-	cout << "5 - Print physicians" << endl;
-	cout << "6 - Print patient's physicians" << endl;
-	cout << "7 - Print employee earnings" << endl;
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			break;
+		case '7':
+			break;
+		default:
+			break;
+		}
+	}while(choice!='q');
 	return 0;
 }
